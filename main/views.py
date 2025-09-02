d156d7a8a5c4d4b1ec5e0d8df64fcde82963922e
Samonto Karmaker <samonto0726@gmail.com>
2025-09-02 17:40:13 +0600

    Implement user registration functionality with form handling and update URL routing

--- main/views.py
+++ main/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import RegisterForm
+from django.contrib.auth import login
 
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
