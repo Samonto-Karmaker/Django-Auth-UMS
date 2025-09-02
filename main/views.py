d7b26da322aecdf1dd18e964e10cd1de90d6702f
Samonto Karmaker <samonto0726@gmail.com>
2025-09-02 18:49:04 +0600

    Add login_required decorator to create_post view for authentication enforcement

--- main/views.py
+++ main/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'main/home.html')
 
+@login_required(login_url='login')
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
